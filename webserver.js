ed1e7c0d435445bb2203b27ae2a1a69c35f5fd4c
o.shandra <oleghsandra@gmail.com>
2019-08-07 19:22:08 +0300

    First commit

---
+++ webserver.js
@@ -0,0 +1,24 @@
+var http = require('http').createServer(handler); //require http server,and create server with function handler()
+
+var fs = require('fs'); //require filesystem module
+http.listen(8181); //listen to port 8181
+
+function handler (req, res) { //create server
+
+fs.readFile(__dirname + '/public/index.html', function(err, data) { //readfile index.html in public folder
+
+    if (err) {
+
+      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+      return res.end("404 Not Found");
+    } 
+
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+
+    res.write(data); //write data from index.html
+
+    return res.end();
+
+  });
+
+}
